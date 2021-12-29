cae43e076662f435af9661ff490e8f8b266ed3cb
loxandra1 <ruksancelik1@gmail.com>
2021-12-29 01:52:08 -0500

    FeatureAWork continue

--- src/test/java/featureAWork/FeatureAWork.java
+++ src/test/java/featureAWork/FeatureAWork.java
@@ -3,5 +3,8 @@ package featureAWork;
 public class FeatureAWork {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
     }
 }
