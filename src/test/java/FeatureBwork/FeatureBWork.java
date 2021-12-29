0d7d1ae5f8b9ef37456ffc60895f9c95015f3fc8
loxandra1 <ruksancelik1@gmail.com>
2021-12-29 02:06:19 -0500

    Featureb Work 2 started

--- src/test/java/FeatureBwork/FeatureBWork.java
+++ src/test/java/FeatureBwork/FeatureBWork.java
@@ -3,5 +3,8 @@ package FeatureBwork;
 public class FeatureBWork {
     public static void main(String[] args) {
         System.out.println("Feature B work");
+        System.out.println("Feature B work");
+
+
     }
 }
